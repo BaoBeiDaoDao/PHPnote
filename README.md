7ff8b66a993452504e3ffc6617e510352f1e8472
宝贝刀刀 <625117069@qq.com>
2016-12-13 13:02:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# PHP note
+
+---
+
+[TOC]
+
+---
+
+## OverView
+
+### Author
+
+**BaoBeiDaoDao**
+
+### Description
+
+PHP 语法学习笔记
+
+---
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-12-13
+ * Time: 12:45
+ */
+
+echo 'hello world!';
+echo "\n";
+echo '世界，你好！';
